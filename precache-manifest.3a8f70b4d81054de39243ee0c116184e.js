755ad136798d42efecf33787968c90cffa2f30f5
Naiara <naiara@Naiaras-MacBook-Pro.local>
2020-04-30 23:42:24 -0600

    Updates

---
+++ precache-manifest.3a8f70b4d81054de39243ee0c116184e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c2713a27e24f3f618197ac295f749ff",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "d23bdd14d03f9d7e92fc",
+    "url": "/my-app/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "50991621c5e82b9fc186",
+    "url": "/my-app/static/css/main.e6ca6df7.chunk.css"
+  },
+  {
+    "revision": "d23bdd14d03f9d7e92fc",
+    "url": "/my-app/static/js/2.0c4a3458.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/my-app/static/js/2.0c4a3458.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50991621c5e82b9fc186",
+    "url": "/my-app/static/js/main.07a59fda.chunk.js"
+  },
+  {
+    "revision": "7017a0a3783b2a9ce739",
+    "url": "/my-app/static/js/runtime-main.b51f6de3.js"
+  }
+]);
