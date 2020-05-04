a6c3fbf6b447e93753c81466ca14f50b4018d89f
Naiara <naiara@Naiaras-MacBook-Pro.local>
2020-05-04 15:32:43 -0600

    add project

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import { Grid } from 'react-mdl';
+import { Card, CardTitle, CardText, CardActions, Button, Grid } from 'react-mdl';
 
 class Projects extends Component {
     render () {
         return(
-            <div style={{margin: 'auto', width: '100%'}}>
-             <Grid className="resume-grid">
-                 <img 
-                     src="https://static.wixstatic.com/media/709944_b0e366f84440466fac3cbe131ea230b4~mv2.gif"
-                     alt="workinprogress"
-                     className="workinprogress-img"
-                 />
-             </Grid>
-            </div>
+            <Grid className="project-style" >
+             <Card shadow={1} style={{display:'flex',  justifyContent:'center', width: '70%', height: '350px', marginTop: '50px', marginLeft: '15%', marginBottom: '20%'}}>
+              <CardTitle expand style={{color: '#fff', background: 'url(https://i.ya-webdesign.com/images/dogs-and-cats-png-8.png)  100%  #46B6AC'}}>Adopt a pet</CardTitle>
+                <CardText>
+                A website that shows some shelter in Calgary city, and promote the importance of adopting a pet instead of buying! 
+                </CardText>
+              <CardActions border>
+                  <Button colored >
+                  <a href="https://nailopes.github.io/comit-project/" target="_blank" rel="noopener noreferrer">
+                  Go to the website
+                  </a>
+                  </Button>
+              </CardActions>
+             </Card>
+            </Grid>
         )
     }
 }
